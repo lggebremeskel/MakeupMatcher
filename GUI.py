97a0bfe2bbd427ea05b8c6714e2d6a0e40752d72
unknown <lggebremeskel@gmail.com>
2021-04-23 23:47:40 -0400

    PEP 8 formatiing

--- GUI.py
+++ GUI.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Makeupdatabasegenerator as mk
 
+
 def openfilename():
     '''
     This function will intiniate a dialog box to open a PNG image. 
     '''
-    filename = filedialog.askopenfilename(title ='"Image', filetypes = [("PNG files", "*.PNG")])
+    filename = filedialog.askopenfilename(
+        title='"Image', filetypes=[("PNG files", "*.PNG")])
     return filename
+
 
 def analyzer(picture):
     '''
@@ -30,17 +33,19 @@ def analyzer(picture):
     shade_values_blue = []
     for i in range(width):
         for j in range(height):
-            shade_pixel = shade.getpixel((i,j))
+            shade_pixel = shade.getpixel((i, j))
             if shade_pixel[0] != 255:
                 if shade_pixel[1] != 255:
                     if shade_pixel[2] != 255:
                         shade_values_red.append(shade_pixel[0])
                         shade_values_green.append(shade_pixel[1])
                         shade_values_blue.append(shade_pixel[2])
-    shade_number = (int(np.mean(shade_values_red)), int(np.mean(shade_values_green)), int(np.mean(shade_values_blue)))
+    shade_number = (int(np.mean(shade_values_red)), int(
+        np.mean(shade_values_green)), int(np.mean(shade_values_blue)))
     return shade_number
 
-def close(brand,shade):
+
+def close(brand, shade):
     '''
     This takes in the RGB value of an image and the database with the shade options for the desired make-ip brand and 
     matches the image shade to the closest shade in the database.
@@ -51,7 +56,8 @@ def close(brand,shade):
         shade: *array*
             an array of the R,G and B values of the image inputted.
     '''
-    return brand[min(range(len(brand)), key = lambda i: ((abs(brand[i][1][0] - shade[0])) + (abs(brand[i][1][1] - shade[1])) + (abs(brand[i][1][2] - shade[2]))))]
+    return brand[min(range(len(brand)), key=lambda i: ((abs(brand[i][1][0] - shade[0])) + (abs(brand[i][1][1] - shade[1])) + (abs(brand[i][1][2] - shade[2]))))]
+
 
 class user_inf(tk.Frame):
     def __init__(self, master):
@@ -73,6 +79,7 @@ class user_inf(tk.Frame):
         This opens the example image.
         '''
         Image.open('Example.JPG').show()
+
     def image_opener(self):
         '''
         This will open the image the user uploaded
@@ -92,9 +99,9 @@ class user_inf(tk.Frame):
         pic = Image.open(self.picture)
         pic = pic.resize((50, 50), Image.ANTIALIAS)
         pic = ImageTk.PhotoImage(pic)
-        self.view_img = Label(self.master, image = pic)
-        self.view_img.place(x = 450, y = 150)
-        self.view_img.image = pic 
+        self.view_img = Label(self.master, image=pic)
+        self.view_img.place(x=450, y=150)
+        self.view_img.image = pic
 
     def compare(self, picture, brand):
         '''
@@ -111,52 +118,61 @@ class user_inf(tk.Frame):
         try:
             shade = analyzer(picture)
             if brand == 'Fenty':
-                fenty_recommend = close(mk.Fenty_database,shade)
+                fenty_recommend = close(mk.Fenty_database, shade)
                 fenty_shade = fenty_recommend[0]
-                self.fenty_shade_result = Label(self.master,text = 'Your recommended Fenty shade is, ' + fenty_shade, fg = 'green')
-                self.fenty_shade_result.place(x = 100, y = 300)
+                self.fenty_shade_result = Label(
+                    self.master, text='Your recommended Fenty shade is, ' + fenty_shade, fg='green')
+                self.fenty_shade_result.place(x=100, y=300)
                 return fenty_shade
             if brand == 'Lancome':
-                lancome_recommend = close(mk.Lancome_database,shade)
+                lancome_recommend = close(mk.Lancome_database, shade)
                 lancome_shade = lancome_recommend[0]
-                self.lancome_shade_result = Label(self.master,text = 'Your recommended Lancome shade is, ' + lancome_shade, fg = 'blue')
-                self.lancome_shade_result.place(x = 100, y = 350)
+                self.lancome_shade_result = Label(
+                    self.master, text='Your recommended Lancome shade is, ' + lancome_shade, fg='blue')
+                self.lancome_shade_result.place(x=100, y=350)
                 return lancome_shade
             if brand == 'Dior':
-                dior_recommend = close(mk.Dior_database,shade)
+                dior_recommend = close(mk.Dior_database, shade)
                 dior_shade = dior_recommend[0]
-                self.dior_shade_result = Label(self.master,text = 'Your recommended Dior shade is, ' + dior_shade, fg = 'brown')
-                self.dior_shade_result.place(x = 100, y = 400)
+                self.dior_shade_result = Label(
+                    self.master, text='Your recommended Dior shade is, ' + dior_shade, fg='brown')
+                self.dior_shade_result.place(x=100, y=400)
                 return dior_shade
         except AttributeError:
-            self.error_msg = Label (self.master, text = 'Error: Please upload a .PNG image first.', fg = 'red' )
-            self.error_msg.place(x = 285, y = 450)
-        
+            self.error_msg = Label(
+                self.master, text='Error: Please upload a .PNG image first.', fg='red')
+            self.error_msg.place(x=285, y=450)
+
     def widgets(self):
         '''
             This places the widgets for the class.
         '''
-        self.welcome = Label(self.master,text = 'Welcome!', fg = 'black')
-        self.welcome.place(x = 350, y = 50)
-        self.upload_request = Label(self.master,text = 'Please upload a close up picture of your hand \nClick "Example Button" to see example image', fg = 'black')
-        self.upload_request.place(x = 200, y = 100)
-        self.exp_img = Button(self.master, text = 'Example' , fg = 'red', command = lambda: self.example_img())
-        self.exp_img.place(x = 450, y = 110)
-        self.up_img = Button(self.master, text = 'Upload Image' , command = lambda: self.image_opener())
-        self.up_img.place(x = 340, y = 150)
-        self.fenty = Button(self.master, text='Fenty', fg='blue', command = lambda: self.compare(self.picture, 'Fenty'))
-        self.fenty.place(x = 200, y = 250)
-        self.lancome = Button(self.master, text='Lancome', fg='blue', command = lambda: self.compare(self.picture, 'Lancome'))
-        self.lancome.place(x = 350, y = 250)
-        self.dior = Button(self.master, text='Dior', fg='blue', command = lambda: self.compare(self.picture,'Dior'))
-        self.dior.place(x = 500, y = 250)
+        self.welcome = Label(self.master, text='Welcome!', fg='black')
+        self.welcome.place(x=350, y=50)
+        self.upload_request = Label(
+            self.master, text='Please upload a close up picture of your hand \nClick "Example Button" to see example image', fg='black')
+        self.upload_request.place(x=200, y=100)
+        self.exp_img = Button(self.master, text='Example',
+                              fg='red', command=lambda: self.example_img())
+        self.exp_img.place(x=450, y=110)
+        self.up_img = Button(self.master, text='Upload Image',
+                             command=lambda: self.image_opener())
+        self.up_img.place(x=340, y=150)
+        self.fenty = Button(self.master, text='Fenty', fg='blue',
+                            command=lambda: self.compare(self.picture, 'Fenty'))
+        self.fenty.place(x=200, y=250)
+        self.lancome = Button(self.master, text='Lancome', fg='blue',
+                              command=lambda: self.compare(self.picture, 'Lancome'))
+        self.lancome.place(x=350, y=250)
+        self.dior = Button(self.master, text='Dior', fg='blue',
+                           command=lambda: self.compare(self.picture, 'Dior'))
+        self.dior.place(x=500, y=250)
+
+
 if __name__ == '__main__':
     mk.initialize_databases()
-    root =Tk()
+    root = Tk()
     root.title("Make-up Matcher")
     root.geometry("700x500")
     app = user_inf(master=root)
     root.mainloop()
-
-
-
--- Makeupdatabasegenerator.py
+++ Makeupdatabasegenerator.py
@@ -2,10 +2,11 @@ from PIL import Image
 import numpy as np
 import os
 
-Lancome_database = [] 
+Lancome_database = []
 Dior_database = []
 Fenty_database = []
-#global variables to store each available shade with their RGB values
+# global variables to store each available shade with their RGB values
+
 
 def shade_value(filename):
     '''
@@ -24,16 +25,19 @@ def shade_value(filename):
     shade_values_blue = []
     for i in range(width):
         for j in range(height):
-            shade_pixel = shade.getpixel((i,j))
+            shade_pixel = shade.getpixel((i, j))
             if shade_pixel[0] != 255:
                 if shade_pixel[1] != 255:
                     if shade_pixel[2] != 255:
                         shade_values_red.append(shade_pixel[0])
                         shade_values_green.append(shade_pixel[1])
                         shade_values_blue.append(shade_pixel[2])
-    shade_number = (int(np.mean(shade_values_red)), int(np.mean(shade_values_green)), int(np.mean(shade_values_blue)))
+    shade_number = (int(np.mean(shade_values_red)), int(
+        np.mean(shade_values_green)), int(np.mean(shade_values_blue)))
     return shade_number
-def directory_reader(folder_path,database):
+
+
+def directory_reader(folder_path, database):
     '''
     This takes in the directory of where the images representing each shade of the make-up brands are stored and converts them to 
     an array of the filename and the RGB value. 
@@ -48,14 +52,17 @@ def directory_reader(folder_path,database):
             shade = shade_value(folder_path + '\\' + i)
             shade_name = i.split(".PNG")[0]
             database_entry = (shade_name, shade)
-            database.append(database_entry)  
+            database.append(database_entry)
+
+
 def initialize_databases():
     '''
     This function initializes the creation of the global variables that store each shade for all three make-up types.
     '''
     current_directory = os.getcwd()
-    directory_reader(current_directory + "\\Foundations\FENTY PRO FILTR SOFT MATTE",Fenty_database)
-    directory_reader(current_directory + "\\Foundations\LANCOME TEINT IDOLE ULTRA 24H LONG WEAR FOUNDATION",Lancome_database)
-    directory_reader(current_directory + "\\Foundations\DIOR FOREVER",Dior_database)
-
-
+    directory_reader(current_directory +
+                     "\\Foundations\FENTY PRO FILTR SOFT MATTE", Fenty_database)
+    directory_reader(current_directory +
+                     "\\Foundations\LANCOME TEINT IDOLE ULTRA 24H LONG WEAR FOUNDATION", Lancome_database)
+    directory_reader(current_directory +
+                     "\\Foundations\DIOR FOREVER", Dior_database)
